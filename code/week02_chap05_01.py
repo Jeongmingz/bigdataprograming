db0e2609a65f804583313dda1e39e42c37715a8c
Jeongmin <joungmin0861@naver.com>
2023-03-09 14:46:30 +0900

    update week02_chap05_01.py
    update week02_chap05_01.py
    1. using split def
    2. change split's parameter

--- code/week02_chap05_01.py
+++ code/week02_chap05_01.py
@@ -1,7 +1,11 @@
 letters = 'abcdefghijklmnopqrstuvwxyz'
+# split => str -> list | default = space
+daelim = 'Daelim University'.split('i')
 
+print(daelim)
 
 print(letters[1:])
-print(letters[:-1]) # == print(letters[:28])
+# == print(letters[:28])
+print(letters[:-1])
 print(letters[8:15])
 print(str(letters[8:15])[::-1])
