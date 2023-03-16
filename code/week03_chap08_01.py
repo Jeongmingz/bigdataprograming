4aa6f11bd22064048f3e5fcd78f68e91ae2431ca
Jeongmin <joungmin0861@naver.com>
2023-03-16 16:28:53 +0900

    week03_chap08_01 : add dict comprehensions

--- code/week03_chap08_01.py
+++ code/week03_chap08_01.py
@@ -3,3 +3,8 @@ acme_customer = dict(first='Wile', middle='E', last='Coyote')
 print(acme_customer)
 
 print(acme_customer.get('hello'))
+
+
+word = 'letters'
+letter_counts = {letter: word.count(letter) for letter in word}
+print(letter_counts)
