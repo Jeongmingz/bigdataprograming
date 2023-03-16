f649cc6e01c0fe02894350f4dff187e2ea0cc8f4
Jeongmin <joungmin0861@naver.com>
2023-03-16 15:20:56 +0900

    week03_chap07_04 : add condition

--- code/week03_chap07_04.py
+++ code/week03_chap07_04.py
@@ -1,9 +1,14 @@
-# List Comprehension
+# # List Comprehension
+# numbers = []
+# for i in range(1, 10, 3):
+#     numbers.append(i**2)
+#
+# print(numbers)
+#
+# numbers = [i**2 for i in range(1, 10, 3)]
+# print(numbers)
+
 numbers = []
-for i in range(1, 10, 3):
-    numbers.append(i**2)
 
-print(numbers)
-
-numbers = [i**2 for i in range(1, 10, 3)]
+numbers = [i**2 for i in range(1, 10) if i % 2 == 0]
 print(numbers)
