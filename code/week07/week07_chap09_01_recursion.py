faec6d5d734aa265b271cebf252f30558582390f
Jeongmin <joungmin0861@naver.com>
2023-04-13 14:46:39 +0900

    fibonacci recursion, repetition version

--- code/week07/week07_chap09_01_recursion.py
+++ code/week07/week07_chap09_01_recursion.py
@@ -15,7 +15,7 @@ def Fibonacci_recursion(n):
 
 def fibonacci_repetition(n):
     """
-    using repeat function 'for' , fibonacci number return
+    using repetition function 'for' , fibonacci number return
     :param n: input number
     :return: fibonacci number
     """
@@ -23,11 +23,12 @@ def fibonacci_repetition(n):
         return n
     else:
         result, t1, t2 = 1, 1, 0
-        for i in range(2, n+1):
+        for i in range(2, n + 1):
             result = t1 + t2
             t2 = t1
             t1 = result
     return result
+
 
 frep = my_utility.time_check(fibonacci_repetition)
 frec = my_utility.time_check(Fibonacci_recursion)
