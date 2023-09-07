5f91eda78935b3030e6fc716a1d6e89d7252512f
Jeongmingz <joungmin0861@naver.com>
2023-09-07 16:18:59 +0900

    marge

--- code/2023-2/week02/week02_numpy.py
+++ code/2023-2/week02/week02_numpy.py
@@ -3,7 +3,7 @@ import random
 
 # :)
 num = int(input('숫자 입력 : '))
-list_ = [random.randint(1,100) for _ in range(num)]
+list_ = [random.randint(1, 100) for _ in range(num)]
 
 ndarray_ = np.array(list_, dtype='int16')
 print(ndarray_)
