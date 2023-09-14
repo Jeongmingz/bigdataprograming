4365a8c029d2dabbc2d2fb68439b07a2c98a980b
Jeongmingz <joungmin0861@naver.com>
2023-09-14 14:36:17 +0900

    Exception 처리

--- code/2023-2/week02/week02_numpy.py
+++ code/2023-2/week02/week02_numpy.py
@@ -1,9 +1,30 @@
 import numpy as np
 import random
+import tkinter as tk  # Built in GUI
 
-# :)
-num = int(input('숫자 입력 : '))
-list_ = [random.randint(1, 100) for _ in range(num)]
+def click_button():
+    n = int(en_number.get())
+    l = [random.randint(1, 100) for i in range(n)]
+    v = np.array(l, dtype='int16')
+    lbl_result.config(text=v)
 
-ndarray_ = np.array(list_, dtype='int16')
-print(ndarray_)
+
+window = tk.Tk()
+window.title('numpy gui version v0.7')
+window.geometry('300x150')
+
+# create widget
+lbl_result = tk.Label(text="random numpy array")
+en_number = tk.Entry()
+btn_click = tk.Button(text="click me!", command=click_button)
+
+# widget layout
+lbl_result.pack()
+en_number.pack(fill='x')
+btn_click.pack(fill='x')
+
+window.mainloop()
+# n = int(input("input number : "))
+# l = [random.randint(1, 100) for i in range(n)]
+# v = np.array(l, dtype='int16')
+# print(v)
