de35ccbb3ebe2ed7d587dcaa7154017becfe3a3c
Jeongmingz <joungmin0861@naver.com>
2023-10-05 14:35:58 +0900

    week06

--- code/2023-2/week06/userLib/jmlearn.py
+++ code/2023-2/week06/userLib/jmlearn.py
@@ -1,14 +1,31 @@
+import numpy as np
+
 
 class LinearRegression:
 	def __init__(self):
 		self.slope = None
 		self.intercept = None
 
+	def fit(self, X, y) -> None:
+		"""
+		Modeling Function
+		:param X: Independent Variable (2d Array format)
+		:param y: Dependent Variable (2d Array format)
+		:return: None
+		"""
+		X_mean = np.mean(X)
+		y_mean = np.mean(y)
 
-	def fit(self, X, y):
-		pass
+		denominator = np.sum(pow((X - X_mean), 2))
+		numerator = np.sum((X - X_mean) * (y - y_mean))
 
-	def preidct(self, X):
-		pass
+		self.slope = numerator / denominator
+		self.intercept = y_mean - (self.slope * X_mean)
 
-
+	def predict(self, X) -> list:
+		"""
+		Predict Value for input
+		:param X: New Independent Variable (Single Value)
+		:return: Predict Value for input (2d format)
+		"""
+		return self.slope * np.array(X) + self.intercept
